3a599b7d04d86b5087c341e16014ac29f6d3d34d
RituYadav92 <40523539+RituYadav92@users.noreply.github.com>
2020-05-21 12:07:59 +0200

    Update config.py

--- apollo/Scraper/config.py
+++ apollo/Scraper/config.py
@@ -11,5 +11,6 @@ Code modified from : https://github.com/egbertbouman/youtube-comment-downloader
 YOUTUBE_VIDEO_URL = 'https://www.youtube.com/watch?v={youtube_id}'
 YOUTUBE_COMMENTS_AJAX_URL_OLD = 'https://www.youtube.com/comment_ajax'
 YOUTUBE_COMMENTS_AJAX_URL_NEW = 'https://www.youtube.com/comment_service_ajax'
+OUTPUT_PATH= "/home/MG10/Desktop/Scraping_script/All_output/"
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
