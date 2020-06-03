3b5f1c1da2b7387f35d6d99d5e94fb2c9ccd255e
Mishra <mha@iis.fhg.de>
2020-06-03 19:09:34 +0200

    [add] some css magic

--- apollo/Frontend/app.py
+++ apollo/Frontend/app.py
@@ -12,6 +12,7 @@ import time
 import pandas as pd
 import tempfile
 import datetime
+from collections import defaultdict 
 
 
 
@@ -37,6 +38,8 @@ COMPLETED = False
 LIMIT = 50
 LOG_RESULT_DATA = None
 LOG_DATA = ''
+DATA_STORE = defaultdict(list)
+
 
 COMMENTS_STORE = []
 
@@ -84,7 +87,9 @@ def scrapper_v2(youtube_id, sensitivity, limit):
         global LOG_RESULT_DATA
         global LOG_DATA
         global COMMENTS_STORE
+        global DATA_STORE
         
+        filename = '{}_{}_{}.csv'.format(youtube_id, sensitivity, limit)
 
         CHART_DATA = [0, 0]
         COMPLETED = False
@@ -117,37 +122,45 @@ def scrapper_v2(youtube_id, sensitivity, limit):
             for comment in download_comments(youtube_id):
                 comment_content = comment['content']
                 score = inference_v2(comment_content, sensitivity)
-                if score > (sensitivity):
-                    toxic_count += 1
-                else:
-                    nontoxic_count +=1
-                CHART_DATA = [(toxic_count / (toxic_count + nontoxic_count)) * 100, (nontoxic_count / (toxic_count + nontoxic_count)) * 100]
-                LOG_DATA = ' '.join(['[' + str(count + 1) + ']', str(comment_content), 'score: ' + str(score)])
-
-                COMMENTS_STORE.append([comment['content'], comment['time'], comment['author'], comment['votes'], comment['photo'], str(score)])
-
+                count += 1
                 count_list.append(count)
                 comment_list.append(comment_content)
                 score_list.append(score)
                 sensitivity_list.append(sensitivity)
 
-                print(comment_content, toxic_count, nontoxic_count, score)
-                count += 1
-                sys.stdout.write("Downloaded %d comment(s)\r" % count)
-                sys.stdout.flush()
+                if score > (sensitivity):
+                    toxic_count += 1
+                else:
+                    nontoxic_count +=1
+                # CHART_DATA = [(toxic_count / (toxic_count + nontoxic_count)) * 100, (nontoxic_count / (toxic_count + nontoxic_count)) * 100]
+                CHART_DATA = [toxic_count, nontoxic_count]
+                LOG_DATA = ' '.join(['[' + str(count) + ']', str(comment_content), 'score: ' + str(score)])
+                EXTRA_LOG_DATA = [comment['content'], comment['time'], comment['author'], comment['votes'], comment['photo'], str(score)]
+                COMMENTS_STORE.append(EXTRA_LOG_DATA)
                 
+
                 if limit and count >= limit:
                     COMPLETED = True
+                    DATA_STORE[youtube_id].append({'chart_data':CHART_DATA, 'extra_log_data': EXTRA_LOG_DATA, 'task_finished': True, 'success': True, 'index': count, 'filename': filename})
                     df['id'], df['comment'], df['score'], df['sensitivity'] = count_list, comment_list, score_list, sensitivity_list
-                    filename = '{}_{}_{}.csv'.format(youtube_id, sensitivity, limit)
                     LOG_RESULT_DATA = filename
                     filepath = os.path.abspath(os.path.join('./', 'downloads', filename))
                     df.to_csv(filepath, encoding='utf-8')
                     break
+                else:
+                    DATA_STORE[youtube_id].append({'chart_data':CHART_DATA, 'extra_log_data': EXTRA_LOG_DATA, 'task_finished': False, 'success': True, 'index': count, 'filename': filename})
+
+
+
+                print(comment_content, toxic_count, nontoxic_count, score)
+                sys.stdout.write("Downloaded %d comment(s)\r" % count)
+                sys.stdout.flush()
+                
             print("\nDone!")
 
         else:
             print(f"The provided YouTube ID : {youtube_id} is invalid! ")
+            DATA_STORE[youtube_id].append({'chart_data':[], 'extra_log_data': [], 'task_finished': True, 'success': False, 'index': -1, 'filename': ''})
             LOG_DATA = 'error'
             COMPLETED = True
             CHART_DATA = [0, 0]
@@ -158,8 +171,8 @@ def scrapper_v2(youtube_id, sensitivity, limit):
         sys.exit(1)
 
 
-@app.route('/chart-data')
-def chart_data():
+@app.route('/chart2-data')
+def chart2_data():
     def send_processed_data():
         global COMPLETED
         while True:
@@ -170,6 +183,21 @@ def chart_data():
             time.sleep(1)
 
     return Response(send_processed_data(), mimetype='text/event-stream')
+
+
+@app.route('/chart-data', methods=['GET'])
+def chart_data():
+    if request.method == 'GET':
+        url = request.args.get('url')
+        youtube_id = extract_id(url)
+
+    def send_data(youtube_id):
+        while len(DATA_STORE[youtube_id])>0:
+            json_data = json.dumps(DATA_STORE[youtube_id].pop(0))
+            yield f"data:{json_data}\n\n"
+            time.sleep(1)
+
+    return Response(send_data(youtube_id), mimetype='text/event-stream')
 
 
 @app.route('/log-data')
@@ -210,6 +238,7 @@ def home():
 
 @app.route('/predict',methods=['GET', 'POST'])
 def predict():
+    print('STARTING PREDICTION*********************')
     '''
     For rendering results on HTML GUI
     '''
@@ -238,6 +267,7 @@ def predict():
     scrapper_v2(COMMENT_LINK, SENSITIVITY, LIMIT)
     
     return jsonify(msg='scraping successfully', status='success')
+
 
 
 if __name__ == "__main__":
